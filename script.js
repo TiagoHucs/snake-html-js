73e729839f2f7ce0760ccdcf67f7b94124740556
TiagoHucs <tiagohucs@gmail.com>
2020-06-07 16:51:24 -0300

    ajustes com Lucas Rezende github/lucascrezende

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-var pedra1;
-var snake = [];
+var snake;
 var t = 0;
+var colidiu = false;
+var comida;
+
+var KEY_UP = 38;
+var KEY_DOWN = 40;
+var KEY_LEFT = 37;
+var KEY_RIGHT = 39;
+var KEY_SPACE = 32;
+
+var speedX = 0;
+var speedY = 0;
+
+var GAME_AREA = [480,280];
+
+var TAM = 20;
 
 function startGame() {
-	pedra1 = new quadrado(19, 19, "white", 60, 20);
-	snake.push(pedra1);
+	restartPosicoes();
 	myGameArea.start();
 }
 
-function restart() {
-	var snake = [];
-	pedra1 = new quadrado(19, 19, "white", 60, 20);
-	snake.push(pedra1);
+function restartPosicoes(){
+	snake = [];
+	t = 0;
+	speedX = 0;
+	speedY = 0;
+	colidiu = false;
+	snake.push(new quadrado(TAM, TAM, "white", 60, 20));
+	criaComida();
 }
 
 var myGameArea = {
 	canvas: document.getElementById("canvas"),
 	start: function () {
-		canvas.width = 480;
-		canvas.height = 270;
+		canvas.width = GAME_AREA[0];
+		canvas.height = GAME_AREA[1];
 		this.context = canvas.getContext("2d");
-		//  document.body.insertBefore(canvas, document.body.childNodes[0]);
 		this.interval = setInterval(updateGameArea, 200);
 	},
 	clear: function () {
@@ -32,107 +48,126 @@ function quadrado(width, height, color, x, y) {
 	this.id = ++t;
 	this.width = width;
 	this.height = height;
-	this.color = color
-	this.speedX = 0;
-	this.speedY = 0;
+	this.color = color;
 	this.x = x;
 	this.y = y;
 
 	this.move = function () {
-		this.x += this.speedX;
-		this.y += this.speedY;
+		this.x += speedX;
+		this.y += speedY;
 	}
 
 	this.paint = function () {
 		myGameArea.context.fillStyle = this.color;
 		myGameArea.context.fillRect(this.x, this.y, this.width, this.height);
 	}
-
 }
 
 function cresce() {
 	x = snake[snake.length - 1].x;
 	y = snake[snake.length - 1].y;
-	snake.push(new quadrado(19, 19, "white", x, y));
+	snake.push(new quadrado(TAM, TAM, "white", x, y));
 }
 
 function saoVizinhos(a,b) {
 	return a - b == 1 || b - a == 1;
 }
 
-function checkColision(pedraA, pedraB) {
-	if (pedraA.x == pedraB.x && pedraA.y == pedraB.y) {
-		pedra1.color = 'red';
-		console.log('pedra:' + pedraA.id + ' colidiu com a pedra:' + pedraB.id);
-		restart()
-	}
-}
-
-
 function updateGameArea() {
-	console.log(snake.length)
+
 	myGameArea.clear();
+
+	// cauda herda posição anterior do quadrado da frente
 	for (var i = snake.length; i > 0; i--) {
 		if (snake[i] && snake[i - 1]) {
 			snake[i].x = snake[i - 1].x;
 			snake[i].y = snake[i - 1].y;
 		}
 	}
+
 	snake.forEach(pinta);
 	function pinta(item, index) {
 		item.paint();
 	}
 
+	comida.paint();
+
+	// checa colisoes entre os quadrados da snake (menos vizinhos)
 	for (var i = 0; i < snake.length; i++) {
 		for (var ii = 0; ii < snake.length; ii++) {
 			if (ii !== i && !saoVizinhos(i,ii)) {
-				checkColision(snake[i], snake[ii]);
+				colidiu = checkColision(snake[i], snake[ii]);
+				console.log('colidiu :' + colidiu );
 			}
 		}
 	}
-	pedra1.move();
-	pedra1.paint();
+
+	// checa colisoes entre a cabeça pra crescer
+	if(checkColision(snake[0], comida)){
+		console.log('comeu');
+		cresce();
+		criaComida();
+	}
+
+	if(colidiu){
+		alert('Game over!');
+		restartPosicoes();
+	} else {
+		snake[0].move();
+		snake[0].paint();
+	}
+	
+}
+
+function checkColision(pedraA, pedraB) {
+	if (pedraA.x == pedraB.x && pedraA.y == pedraB.y) {
+		return true;
+	}
+	return false;
+}
+
+function criaComida() {
+	var x = getRndInteger(0,GAME_AREA[0]-TAM);
+	var y = getRndInteger(0,GAME_AREA[1]-TAM);
+	comida = new quadrado(TAM,TAM,"red",x,y);
+}
+
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * ((max - min) / TAM)) * TAM + min;
 }
 
 window.addEventListener('keydown', this.controla, false);
 
 function controla(e) {
-	//alert(e.keyCode);
-	if (e.keyCode == 38 && pedra1.speedY == 0) {
-		pedra1.speedX = 0;
-		pedra1.speedY = -20;
-	} else if (e.keyCode == 40 && pedra1.speedY == 0) {
-		pedra1.speedX = 0;
-		pedra1.speedY = 20;
-	} else if (e.keyCode == 37 && pedra1.speedX == 0) {
-		pedra1.speedY = 0;
-		pedra1.speedX = -20;
-	} else if (e.keyCode == 39 && pedra1.speedX == 0) {
-		pedra1.speedY = 0;
-		pedra1.speedX = 20;
-	} else if (e.keyCode == 32) {
+
+	if (e.keyCode == KEY_UP) {
+
+		zeraVelocidade();
+		speedY = -20;
+
+	} else if (e.keyCode == KEY_DOWN) {
+
+		zeraVelocidade();
+		speedY = 20;
+
+	} else if (e.keyCode == KEY_LEFT) {
+
+		zeraVelocidade();
+		speedX = -20;
+
+	} else if (e.keyCode == KEY_RIGHT) {
+
+		zeraVelocidade();
+		speedX = 20;
+		
+	} else if (e.keyCode == KEY_SPACE){
 		cresce();
 	}
 
 }
 
-//pode ser que eu use:
-
-this.crashWith = function (otherobj) {
-	var myleft = this.x;
-	var myright = this.x + (this.width);
-	var mytop = this.y;
-	var mybottom = this.y + (this.height);
-	var otherleft = otherobj.x;
-	var otherright = otherobj.x + (otherobj.width);
-	var othertop = otherobj.y;
-	var otherbottom = otherobj.y + (otherobj.height);
-	var crash = true;
-	if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-		crash = false;
-	} else {
-		console.log('colidiu')
-	}
-
-	return crash;
+function zeraVelocidade(){
+	speedX = 0;
+	speedY = 0;
 }
+
